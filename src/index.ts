76660fa11582e461dc2bb877f7fd53187939ae13
Toms Jansons <toms@graphyapp.com>
2021-11-29 01:20:00 +0200

    added lib

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
-import { Plugin } from 'esbuild'
-import convert from '@buxlabs/amd-to-es6'
-import path from 'path'
-import fs from 'fs'
+import { Plugin } from "esbuild";
+import convert from "@buxlabs/amd-to-es6";
+import path from "path";
+import fs from "fs";
 
-const filter = /\b(?:define)\b/
+const filter = /\b(?:define)\b/;
 
-export const amd = (): Plugin => ({
-	name: 'amd',
-	setup(build) {
-		build.onResolve({ filter }, args => {
-			return {
-				path: path.resolve(args.resolveDir, args.path),
-				namespace: 'amd'
-			}
-		})
-		build.onLoad({ filter: /.*/, namespace: 'amd' }, args => {
-			const result = fs.readFileSync(args.path, 'utf-8')
-			const transformed = convert(result)
-			return {
-				contents: transformed,
-				loader: 'js'
-			}
-		})
-	}
-})
+export function amd(): Plugin {
+  return {
+    name: "amd",
+    setup(build) {
+      build.onResolve({ filter }, (args) => {
+        return {
+          path: path.resolve(args.resolveDir, args.path),
+          namespace: "amd",
+        };
+      });
+      build.onLoad({ filter: /.*/, namespace: "amd" }, (args) => {
+        const result = fs.readFileSync(args.path, "utf-8");
+        const transformed = convert(result);
+        return {
+          contents: transformed,
+          loader: "js",
+        };
+      });
+    },
+  };
+}
